5a7e81b943eaf0080a9fa689ff275cbae46cc99f
Jimmy Li <jimmyli921@gmail.com>
2017-12-08 19:00:04 -0800

    Fix styling

--- frontend/components/post_list_item.jsx
+++ frontend/components/post_list_item.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 const PostListItem = ({post}) => {
   return (
-    <li>
-      <div>
-        <img src={post.thumbnail}/>
-      </div>
-      <div>
-        {post.score} - <a href={post.url}>{post.title}</a>
+    <li className="post-list-item h-box">
+      <div className="score-wrapper flex-center"><h3>{post.score}</h3></div>
+        {
+          !post.thumbnail || post.thumbnail === "self" ?
+          null :
+          <div className="post-img-wrapper">
+            <img src={post.thumbnail}/>
+          </div>
+        }
+      <div className="title-info-wrapper v-box">
+        <div><a href={post.url}><h2>{post.title}</h2></a></div>
+        <p>Posted by <span>{post.author}</span></p>
       </div>
     </li>
   );
